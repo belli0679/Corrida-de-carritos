625b05b4fa28980d3812e7d1a5e0e3a2ef92fffd
belli0679 <93786545+belli0679@users.noreply.github.com>
2022-03-11 13:53:22 -0300

    Aula 11-03

--- js/Form.js
+++ js/Form.js
@@ -1,13 +1,13 @@
 class Form {
   constructor() {
-    this.input = createInput("").attribute("placeholder", "Digite seu nome");
+    this.input = createInput("").attribute("placeholder", "Digite Seu Nome");
     this.playButton = createButton("Jogar");
-    this.titleImg = createImg("./assets/TITULO.png", "nome do jogo");
+    this.titleImg = createImg("./assets/TITULO.png", "game title");
     this.greeting = createElement("h2");
   }
 
   setElementsPosition() {
-    this.titleImg.position(120,50);
+    this.titleImg.position(120, 50);
     this.input.position(width / 2 - 110, height / 2 - 80);
     this.playButton.position(width / 2 - 90, height / 2 - 20);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
@@ -26,22 +26,25 @@ class Form {
     this.input.hide();
   }
 
+  handleMousePressed() {
+    this.playButton.mousePressed(() => {
+      this.input.hide();
+      this.playButton.hide();
+      var message = `
+      Olá ${this.input.value()}
+      </br>espere o outro jogador entrar...`;
+      this.greeting.html(message);
+      playerCount += 1;
+      player.name = this.input.value();
+      player.index = playerCount;
+      player.addPlayer();
+      player.updateCount(playerCount);
+    });
+  }
+
   display() {
     this.setElementsPosition();
     this.setElementsStyle();
     this.handleMousePressed();
   }
-
-  handleMousePressed(){
-    this.playButton.mousePressed(()=>{
-      this.input.hide();
-      this.playButton.hide();
-      var message = `Uepa, bem vindo ${this.input.value()}</br> a Corrida de Carros, espere o outro jogador entrar.`
-      this.greeting.html(message);
-
-    })
-
-
-  }
-
 }
